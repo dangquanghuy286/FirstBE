7115c932e9b90fa9390a1041e4972271c48b11ce
dangquanghuy286 <154116366+dangquanghuy286@users.noreply.github.com>
2025-07-14 00:40:45 +0700

    Create page Admin

--- index.js
+++ index.js
@@ -1,7 +1,9 @@
 const express = require("express");
-
 require("dotenv").config();
 const db = require("./config/database");
+
+// Sửa đường dẫn import này
+const routeAdmin = require("./routes/admin/index.route");
 const route = require("./routes/client/index.route");
 
 db.connect();
@@ -11,10 +13,10 @@ const port = process.env.PORT;
 
 app.set("views", "./views");
 app.set("view engine", "pug");
-
 app.use(express.static("public"));
 
 route(app);
+routeAdmin(app);
 
 app.listen(port, () => {
   console.log(`Server is running on http://localhost:${port}`);
---
+++ routes/admin/index.route.js
@@ -0,0 +1,7 @@
+const systemConfig = require("../../config/system");
+
+const dashboardRoute = require("./dashboard.route");
+
+module.exports = (app) => {
+  app.use(`${systemConfig.prefixAdmin}/dashboard`, dashboardRoute);
+};
