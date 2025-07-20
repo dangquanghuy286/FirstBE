0afc3e6b06df0aacfa33232c3dfced95924b5b7b
dangquanghuy286 <154116366+dangquanghuy286@users.noreply.github.com>
2025-07-20 16:44:54 +0700

    Preview

--- public/admin/js/script.js
+++ public/admin/js/script.js
@@ -134,3 +134,17 @@ if (formChangeMulti) {
     }
   });
 }
+
+// Preview Image
+const uploadImg = document.querySelector("[upload-image]");
+if (uploadImg) {
+  const uploadImageInput = document.querySelector("[upload-image-input]");
+  const uploadImagePreview = document.querySelector("[upload-image-preview]");
+
+  uploadImageInput.addEventListener("change", (e) => {
+    const file = e.target.files[0];
+    if (file) {
+      uploadImagePreview.src = URL.createObjectURL(file);
+    }
+  });
+}
