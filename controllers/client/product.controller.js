5adc834e03f9409329b982b3948ff53060a7b69c
dangquanghuy286 <154116366+dangquanghuy286@users.noreply.github.com>
2025-07-21 16:51:14 +0700

    feat: detailItem Client

--- controllers/client/product.controller.js
+++ controllers/client/product.controller.js
@@ -1,10 +1,33 @@
-const product = require("../../models/product.model");
+const Product = require("../../models/product.model");
 
 module.exports.index = async (req, res) => {
-  const products = await product.find({
+  const products = await Product.find({
     status: "active",
     deleted: false,
-  });
-
+  }).sort({ position: "desc" });
   res.render("client/pages/product/index", { products });
+};
+
+// [GET]/products/:slug
+module.exports.detail = async (req, res) => {
+  try {
+    const find = {
+      deleted: false,
+      slug: req.params.slug,
+      status: "active",
+    };
+
+    const productDetail = await Product.findOne(find);
+
+    if (!productDetail) {
+      return res.status(404).render("client/pages/errors/404");
+    }
+
+    res.render("client/pages/product/detail", {
+      product: productDetail,
+    });
+  } catch (error) {
+    console.error("Error fetching product detail:", error);
+    res.status(500).render("client/pages/errors/500");
+  }
 };
