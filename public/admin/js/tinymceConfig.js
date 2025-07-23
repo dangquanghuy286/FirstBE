f139c81adb24791c6270c31f6c20a09ea7678dbe
dangquanghuy286 <154116366+dangquanghuy286@users.noreply.github.com>
2025-07-23 18:29:31 +0700

    refactor :Port Env

--- public/admin/js/tinymceConfig.js
+++ public/admin/js/tinymceConfig.js
@@ -1,6 +1,7 @@
 tinymce.init({
   selector: "textarea#description",
   plugins: "lists link image table code help wordcount",
+  apiKey: window.TINYMCE_API_KEY,
   toolbar:
     "undo redo | formatselect | bold italic backcolor | alignleft aligncenter alignright alignjustify | bullist numlist outdent indent | removeformat | help",
 });
