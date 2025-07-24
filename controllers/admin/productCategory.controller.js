ea1f9159619e4e1961abd1c701f8631bff8fe98c
dangquanghuy286 <154116366+dangquanghuy286@users.noreply.github.com>
2025-07-24 17:09:19 +0700

    fix: category

--- controllers/admin/productCategory.controller.js
+++ controllers/admin/productCategory.controller.js
@@ -12,12 +12,38 @@ module.exports.index = async (req, res) => {
     products: products,
   });
 };
+
 // [GET] /admin/categories-product
 module.exports.create = async (req, res) => {
+  let find = {
+    deleted: false,
+  };
+
+  // Hàm đệ quy để xây dựng cây từ mảng dữ liệu phẳng
+  function createTree(arr, parentId = "") {
+    const tree = [];
+    arr.forEach((item) => {
+      if (item.parent_id === parentId) {
+        const newItem = item;
+        const children = createTree(arr, item._id.toString());
+        if (children.length > 0) {
+          newItem.children = children;
+        }
+        tree.push(newItem);
+      }
+    });
+    return tree;
+  }
+
+  const records = await ProductCategory.find(find);
+  const categoriesTree = createTree(records);
+
   res.render("admin/pages/productsCategory/create", {
-    title: " Create Category",
+    title: "Create Category",
+    records: categoriesTree,
   });
 };
+
 //[POST] /admin/categories-product/create
 module.exports.createItem = async (req, res) => {
   // Tự động tăng position
