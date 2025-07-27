29288f81eae1c2dde62a327636c7e9254d48990a
dangquanghuy286 <154116366+dangquanghuy286@users.noreply.github.com>
2025-07-28 00:54:20 +0700

    feat: View Account

--- controllers/admin/account.controller.js
+++ controllers/admin/account.controller.js
@@ -1,24 +1,58 @@
 const Account = require("../../models/account.model");
+var md5 = require("md5");
+const Role = require("../../models/role.model");
+
 // [GET] /admin/accounts
 module.exports.account = async (req, res) => {
   let find = {
     deleted: false,
   };
-  const records = await Account.find(find);
+  const records = await Account.find(find).select("-password -token");
+  for (const record of records) {
+    const role = await Role.findOne({
+      _id: record.roleId,
+      deleted: false,
+    });
+    record.role = role;
+  }
   res.render("admin/pages/account/index", {
     title: "Account",
     records: records,
   });
+  console.log(records);
 };
 // [GET] /admin/accounts
 module.exports.create = async (req, res) => {
+  const roles = await Role.find({
+    deleted: false,
+  });
   res.render("admin/pages/account/create", {
     title: "Create Account",
+    roles: roles,
   });
 };
 // [POST] /admin/accounts
 module.exports.createPost = async (req, res) => {
-  const records = new Account(req.body);
-  await records.save();
-  res.redirect(req.get("referer") || "/admin/accounts");
+  try {
+    const emailExit = await Account.findOne({
+      email: req.body.email,
+      phone: req.body.phone,
+      deleted: false,
+    });
+
+    if (emailExit) {
+      req.flash("error", `Email hoặc số điện thoại đã tồn tại!`);
+      res.redirect(req.get("referer") || "/admin/accounts.create");
+    } else {
+      req.body.password = md5(req.body.password);
+      const records = new Account(req.body);
+      await records.save();
+      req.flash("success", `Tạo tài khoản thành công!`);
+      res.redirect("/admin/accounts");
+    }
+  } catch (error) {
+    console.error("Lỗi khi tạo tài khoản:", error);
+    req.flash("error", `Có lỗi xảy ra khi tạo tài khoản: ${error.message}`);
+    res.redirect("/admin/accounts");
+  }
 };
