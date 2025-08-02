79cb96bd8fea4b0f334a6f442bcf0e7d36ae72a1
dangquanghuy286 <154116366+dangquanghuy286@users.noreply.github.com>
2025-08-02 20:22:40 +0700

    refactor: Add to Cart

--- middlewares/client/cart.middleware.js
+++ middlewares/client/cart.middleware.js
@@ -10,6 +10,16 @@ module.exports.cartId = async (req, res, next) => {
       expires: new Date(Date.now() + date),
       httpOnly: true,
     });
+  } else {
+    const cart = await Carts.findOne({
+      _id: req.cookies.cartId,
+    });
+
+    cart.totalQuantity = cart.product.reduce(
+      (sum, item) => sum + item.quantity,
+      0
+    );
+    res.locals.miniCart = cart;
   }
   next();
 };
