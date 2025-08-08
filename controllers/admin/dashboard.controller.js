2641fb8e7f4422e66bda4261003e28f26150de26
dangquanghuy286 <154116366+dangquanghuy286@users.noreply.github.com>
2025-08-08 19:23:24 +0700

    feat : dashboard easy

--- controllers/admin/dashboard.controller.js
+++ controllers/admin/dashboard.controller.js
@@ -1,4 +1,84 @@
+const Account = require("../../models/account.model");
+const Product = require("../../models/product.model");
+const ProductCategory = require("../../models/productCategory.model");
+const User = require("../../models/user.model");
+
 // [GET] /admin/dashboard
-module.exports.dashboard = (req, res) => {
-  res.render("admin/pages/dashboard/index", { title: "Login" });
+module.exports.dashboard = async (req, res) => {
+  const statistic = {
+    categoryProduct: {
+      total: 0,
+      active: 0,
+      inactive: 0,
+    },
+    product: {
+      total: 0,
+      active: 0,
+      inactive: 0,
+    },
+    account: {
+      total: 0,
+      active: 0,
+      inactive: 0,
+    },
+    user: {
+      total: 0,
+      active: 0,
+      inactive: 0,
+    },
+  };
+  statistic.categoryProduct.total = await ProductCategory.countDocuments({
+    deleted: false,
+  });
+  statistic.categoryProduct.active = await ProductCategory.countDocuments({
+    status: "active",
+    deleted: false,
+  });
+  statistic.categoryProduct.inactive = await ProductCategory.countDocuments({
+    status: "inactive",
+    deleted: false,
+  });
+  statistic.product.total = await Product.countDocuments({
+    deleted: false,
+  });
+  statistic.product.active = await Product.countDocuments({
+    status: "active",
+    deleted: false,
+  });
+  statistic.product.inactive = await Product.countDocuments({
+    status: "inactive",
+    deleted: false,
+  });
+  statistic.account.total = await Account.countDocuments({
+    deleted: false,
+  });
+  statistic.account.active = await Account.countDocuments({
+    status: "active",
+    deleted: false,
+  });
+  statistic.account.inactive = await Account.countDocuments({
+    status: "inactive",
+    deleted: false,
+  });
+  statistic.user.total = await User.countDocuments({
+    deleted: false,
+  });
+  statistic.user.active = await User.countDocuments({
+    status: "active",
+    deleted: false,
+  });
+  statistic.user.inactive = await User.countDocuments({
+    status: "inactive",
+    deleted: false,
+  });
+
+  statistic.categoryProduct.inactive = await ProductCategory.countDocuments({
+    status: "inactive",
+    deleted: false,
+  });
+
+  res.render("admin/pages/dashboard/index", {
+    title: "Dashboard",
+    statistic,
+  });
 };
