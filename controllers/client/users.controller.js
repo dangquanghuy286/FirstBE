c3126bd6874ec9364893428c5bbb81b57eabcd9e
dangquanghuy286 <154116366+dangquanghuy286@users.noreply.github.com>
2025-08-17 23:10:20 +0700

    feat:refactor

--- controllers/client/users.controller.js
+++ controllers/client/users.controller.js
@@ -1,28 +1,24 @@
 const User = require("../../models/user.model");
 const userSocket = require("../../sockets/client/user.socket");
+
 module.exports.notFriend = async (req, res) => {
-  // Socket
   userSocket(res);
-  // End Socket
 
   const userId = res.locals.user.id;
-
-  const myUser = await User.findOne({
-    _id: userId,
-  });
-
-  const requestFriend = myUser.requestFriends;
-  const acceptFriend = myUser.acceptFriends;
+  const myUser = await User.findOne({ _id: userId });
+  const requestFriend = myUser.requestFriends || [];
+  const acceptFriend = myUser.acceptFriends || [];
 
   const users = await User.find({
-    $and: [
-      { _id: { $ne: userId } },
-      { _id: { $nin: requestFriend } },
-      { _id: { $nin: acceptFriend } },
+    $nor: [
+      { _id: userId },
+      { _id: { $in: requestFriend } },
+      { _id: { $in: acceptFriend } },
     ],
     status: "active",
     deleted: false,
-  }).select("id avatar userName");
+  }).select("_id avatar userName");
+
   res.render("client/pages/users/notFriend", {
     title: "NotFriend",
     users: users,
@@ -34,9 +30,7 @@ module.exports.requestFriend = async (req, res) => {
   userSocket(res);
 
   const userId = res.locals.user.id;
-
   const myUser = await User.findOne({ _id: userId });
-
   const requestFriend = myUser.requestFriends || [];
 
   const users = await User.find({
