2618fcf0d9d13d11818ca16cc5a78e1239105cba
dangquanghuy286 <154116366+dangquanghuy286@users.noreply.github.com>
2025-08-19 23:25:38 +0700

    dfd

--- sockets/client/user.socket.js
+++ sockets/client/user.socket.js
@@ -3,7 +3,8 @@ const User = require("../../models/user.model");
 module.exports = (res) => {
   _io.once("connection", (socket) => {
     socket.on("CLIENT_ADD_FRIEND", async (userId) => {
-      const myUserId = res.locals.user.id;
+      //id của b
+      const myUserId = res.locals.user.id; //Id của a
 
       //   Thêm id của A vào acceptFriends của B
       const exitIdAinB = await User.findOne({
@@ -17,6 +18,21 @@ module.exports = (res) => {
           },
           {
             $push: { acceptFriends: myUserId },
+          }
+        );
+      }
+      // Kiểm tra id của B đã có chưa
+      const exitBinA = await User.findOne({
+        _id: myUserId,
+        requestFriends: userId,
+      });
+      if (!exitBinA) {
+        await User.updateOne(
+          {
+            _id: myUserId,
+          },
+          {
+            $push: { requestFriends: userId },
           }
         );
       }
